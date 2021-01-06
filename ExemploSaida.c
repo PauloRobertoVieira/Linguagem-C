a0b480a725e707995f711293f86e49d336c3faa8
prvieira <prvgyn@gmail.com>
2021-01-06 11:45:24 -0300

    ExemploSaida
    Usando função printf() para exemplificar saídas.

---
+++ ExemploSaida.c
@@ -0,0 +1,18 @@
+/*Escreva um programa em C que apresente a seguinte saída:
+1 -     Clientes
+2 -     Fornecedores
+3 -     Faturas
+0 -     Sair
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("1 -  Clientes\n");
+    printf("2 -  Fornecedores\n");
+    printf("3 -  Faturas\n");
+    printf("0 -  Sair\n");
+
+    return 0;
+}
