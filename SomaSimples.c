2d2314deda08bcc9dfd3cf931859659e7388bd55
prvieira <prvgyn@gmail.com>
2021-01-11 22:28:12 -0300

    Update SomaSimples.c

--- SomaSimples.c
+++ SomaSimples.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include<stdio.h>//biblioteca entrada e saida
 
-int main(void)
+int main(void)//função principal
 {
-    int n1, n2 = 0;
+    int n1, n2 = 0;//declaração variaveis
     printf("Informe dois numeros:\n");
     scanf("%d %d", &n1, &n2);
 
-    printf("A soma de %d + %d = %d\n", n1, n2, n1+n2);
+    printf("A soma de %d + %d = %d\n", n1, n2, n1+n2);//saida dados com calculo
 
     return 0;
 }
