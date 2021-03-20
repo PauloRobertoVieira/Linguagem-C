093364f3b30db9eee8e8a4bfef018bb6350da518
prvieira <prvgyn@gmail.com>
2021-03-19 23:31:17 -0300

    usandoSwitch

--- Cap 3/UsandoSwitch.c
+++ Cap 3/UsandoSwitch.c
@@ -1,35 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
+int main(void)
+{
     int dia = 0;
 
     printf("Qual dia da semana?\n");
     scanf("%d", &dia);
 
-    switch(dia){
-        case 1:
+    switch (dia)
+    {
+    case 1:
         printf("Domingo\n");
         break;
-        case 2:
+    case 2:
         printf("Segunda\n");
         break;
-        case 3:
+    case 3:
         printf("Terça\n");
         break;
-        case 4:
+    case 4:
         printf("Quarta\n");
         break;
-        case 5:
+    case 5:
         printf("Quitna\n");
         break;
-        case 6:
+    case 6:
         printf("Sextou\n");
         break;
-        case 7:
+    case 7:
         printf("Sabado\n");
         break;
+    default:
+        printf("Valor invalido\n");
+        if (dia == default)
+        {
+            printf("Qual dia da semana?\n");
+            scanf("%d", &dia);
+        }
     }
-
     return 0;
-
 }
