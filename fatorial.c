bae6ffd31cc0dbb68d229888fa58cdbcf30d9a57
prvieira <prvgyn@gmail.com>
2021-03-24 10:21:17 -0300

    Fatorial somando as multiplicações

---
+++ fatorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void){
+    int n, i=1,aux=1;
+
+    printf("numero para fatorial: ");
+    scanf("%d",&n);
+    if (n==0){
+        printf("Fatorial de 0 = 1\n");
+    }else
+        while(i<=n){
+            aux = aux*i;
+            printf("Fatorial = %d\n", aux);
+            i++;
+        }
+    return 0;
+}
