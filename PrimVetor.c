2ddd3daf8cb9cb2fa9170f02f15ea6c66f8cd8d5
prvieira <prvgyn@gmail.com>
2021-03-26 15:37:45 -0300

    Primeira implemetnação vetor/array

---
+++ PrimVetor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int vet[5] = {10, 19, 26, 27, 30};
+    int i = 0;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Valor [i]= %d\n", vet[i]);
+    }
+
+    return 0;
+}
