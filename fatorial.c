325b4da438ef1bb9cb7f8c042328e284b5f1f871
prvieira <prvgyn@gmail.com>
2021-03-28 20:15:03 -0300

    Fatorial em uma função

--- fatorial.c
+++ fatorial.c
@@ -1,15 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-    int n, i=1,aux=1;
+int main(void)
+{
+    int n, i = 1, aux = 1;
 
     printf("numero para fatorial: ");
-    scanf("%d",&n);
-    if (n==0){
+    scanf("%d", &n);
+    if (n == 0)
+    {
         printf("Fatorial de 0 = 1\n");
-    }else
-        while(i<=n){
-            aux = aux*i;
+    }
+    else
+        while (i <= n)
+        {
+            aux = aux * i;
             printf("Fatorial = %d\n", aux);
             i++;
         }
